4fa1799791ea76909db7fe7eddeb2ce1d4d5b209
stem-cell <hypnohubstemcell@gmail.com>
2013-10-25 10:02:54 -0700

    Added a function to utils.php

--- server/utils.php
+++ server/utils.php
@@ -6,3 +6,13 @@ function using($name)
 {
     require_once __DIR__."/$name.php";
 }
+
+// I don't blame you 'cause most of the time I don't, either :)
+function youProbablyDontKnowWhatYouAreDoing() {
+    echo nl2br("HypnoHub Hover Zoom ".CHRISTINA_VERSION." API endpoint"
+              ." - please provide a valid query, my young grasshopper. \n\n");
+    echo nl2br("This is what you gave us on <code>\$_GET</code>, see? No valid query data:\n");
+    echo '<pre>';
+    var_dump($_GET);
+    echo '</pre>';
+}
