ddb39151d85b355f0eaf8c635153876eb49141be
stem-cell <hypnohubstemcell@gmail.com>
2013-10-25 08:05:43 -0700

    Cleanup posts.php

--- server/posts.php
+++ server/posts.php
@@ -4,7 +4,8 @@
 class Posts
 {
 	// Get information about a post, by numerical ID.
-	function get($id) {
+	function get($id)
+	{
 	    $pdo = DB::connect();
 	    $query = $pdo->query("SELECT * FROM posts WHERE id = $id");
 	    $row = $query->fetchObject();
@@ -12,11 +13,13 @@ class Posts
 	}
 
 	// Gets a (textual) list of tags for a post as an array. No tags are prefixed, except for rating:N.
-	function getTags($post) {
+	function getTags($post)
+	{
 	    if (!is_array($post) or !isset($post['id'])) return array();
+	    
 	    $pdo = DB::connect();
 	    $id = $post['id'];
-	    $sql = "SELECT name FROM tags JOIN posts_tags ON tag_id = tags.id JOIN posts ON post_id = posts.id WHERE post_id = $id";
+	    $sql = "SELECT name FROM tags INNER JOIN posts_tags ON tag_id = tags.id INNER JOIN posts ON post_id = posts.id WHERE post_id = $id";
 	    $query = $pdo->query($sql);
 	    $rows = $query->fetchAll(PDO::FETCH_NUM);
 	    $list = array_map(function($i) { return $i[0]; }, $rows);
