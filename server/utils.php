2404136d3c220390227a97c9857ddc428ad62edc
stem-cell <hypnohubstemcell@gmail.com>
2013-10-26 07:59:37 -0700

    Included extra utils and corrected the yaml.php code.

--- server/utils.php
+++ server/utils.php
@@ -2,7 +2,57 @@
 
 // Function to include a script inside the Phar.
 // Note that the name does not require the .php extension.
+// If it starts with "../", it is considered a path relative to the Phar.
 function using($name)
 {
-    require_once __DIR__."/$name.php";
+    if (substr($name, 0, 3) == '../')
+    {
+        require_once normalizePath(dirname(dirname(unPhar(__DIR__))).'/'.$name);
+    }
+    else
+    {
+        require_once __DIR__."/$name.php";
+    }
+}
+
+// Normalizes a path, handling .. references that go beyond the initial folder reference.
+// Taken from here: http://www.php.net/manual/en/function.realpath.php#112367
+function normalizePath($path)
+{
+    $parts = array(); // Array to build a new path from the good parts
+    $path = str_replace('\\', '/', $path); // Replace backslashes with forwardslashes
+    $path = preg_replace('/\/+/', '/', $path); // Combine multiple slashes into a single slash
+    $segments = explode('/', $path); // Collect path segments
+    $test = ''; // Initialize testing variable
+
+    foreach($segments as $segment)
+    {
+        if($segment != '.')
+        {
+            $test = array_pop($parts);
+
+            if(is_null($test))
+            {
+                $parts[] = $segment;
+            }
+            else if($segment == '..')
+            {
+                if($test == '..') $parts[] = $test;
+
+                if($test == '..' || $test == '') $parts[] = $segment;
+            }
+            else
+            {
+                $parts[] = $test;
+                $parts[] = $segment;
+            }
+        }
+    }
+    return implode('/', $parts);
+}
+
+// Remove the phar url schema.
+function unPhar($path)
+{
+    return preg_replace('~^phar://~', '', $path);
 }
--- server/yaml.php
+++ server/yaml.php
@@ -2,9 +2,10 @@
 
 // Again, just like with jQuery, we're taking advantage of the fact that,
 // conveniently, MyImoutoBooru is already using tools we'd like to use anyway.
-using('/../../vendor/Symfony/Component/Yaml/Yaml.php');
-using('/../../vendor/Symfony/Component/Yaml/Parser.php');
-using('/../../vendor/Symfony/Component/Yaml/Inline.php');
-using('/../../config/default_config.php');
-using('/../../config/config.php');
+//print_r(scandir(normalizePath(dirname(dirname(unPhar(__DIR__))).'/../')));
+using('../vendor/Symfony/Component/Yaml/Yaml.php');
+using('../vendor/Symfony/Component/Yaml/Parser.php');
+using('../vendor/Symfony/Component/Yaml/Inline.php');
+using('../config/default_config.php');
+using('../config/config.php');
 use Symfony\Component\Yaml\Yaml as SfYaml;
