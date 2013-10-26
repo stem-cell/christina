86f74cb0e7f53fe304af084d852d4f123b9020f7
stem-cell <hypnohubstemcell@gmail.com>
2013-10-26 08:41:47 -0700

    Namespaced the Phar stub.

--- phar-stub.php
+++ phar-stub.php
@@ -1,6 +1,6 @@
-<?php
+<?php namespace Christina;
 
 $pharFilename = basename(__FILE__);
-Phar::mapPhar($pharFilename);
+\Phar::mapPhar($pharFilename);
 require "phar://$pharFilename/server/boot.php";
 __HALT_COMPILER();
