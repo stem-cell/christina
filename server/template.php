af3c4344e3800687bf27720e0f8554d6ac909afd
stem-cell <hypnohubstemcell@gmail.com>
2013-10-29 08:48:54 -0700

    Better template functions.

--- server/template.php
+++ server/template.php
@@ -3,10 +3,18 @@
 // Groups template-related functionality
 class Template
 {
-    // Renders a template with the given variables.
-    static function render($name, $vars)
+    // Renders a template with the given variables, and
+    // returns its rendered result as a string.
+    static function render($name, $vars = [])
     {
         extract($vars);
+        ob_start();
         require dirname(__DIR__)."/templates/$name.php";
+        return ob_get_clean();
+    }
+
+    static function display($name, $vars = [])
+    {
+        echo self::render($name, $vars);
     }
 }
