83e08b2156b87a4c1f815d907e28354ee5e2da85
stem-cell <hypnohubstemcell@gmail.com>
2013-10-30 15:03:29 -0700

    Load route if it is valid.

--- server/logic.php
+++ server/logic.php
@@ -1,63 +1,70 @@
 <?php namespace Christina;
 
 // Main logic. Let's do our thing.
-if (Query::isPost())
+if (Routes::isValid())
 {
-    $id = intval($_GET['post']);
-    $post = Posts::get($id);
-    $blacklist = BlackLists::get(@$_COOKIE['user_id']);
-    $postArray = $post ? (array)$post : array();
-    $tags = Posts::getTags($postArray);
-    
-    try
+    Routes::call();
+}
+else
+{
+    if (Query::isPost())
     {
-        $display = Posts::imageUrl((array)$post);
-    }
-    catch (\Exception $e)
-    {
-        print_r((array)$post);
-        exit;
-        $display = CHRISTINA_404;
-    }
-    
-    if (Query::isJson())
-    {
-        $response = array(
-            'version' => CHRISTINA_VERSION,
-            'success' => !!$post,
-            'display' => $display,
-            'raw' => $postArray,
-            'tags' => $tags
-        );
-        $response['blacklisted'] = Blacklists::check($tags, $blacklist);
-        $json = json_encode($response, JSON_NUMERIC_CHECK | JSON_PRETTY_PRINT);
-        echo $json;
-    }
-    else
-    {
-        if ($post)
+        $id = intval($_GET['post']);
+        $post = Posts::get($id);
+        $blacklist = BlackLists::get(@$_COOKIE['user_id']);
+        $postArray = $post ? (array)$post : array();
+        $tags = Posts::getTags($postArray);
+        
+        try
         {
-            if (Blacklists::check($tags, $blacklist))
+            $display = Posts::imageUrl((array)$post);
+        }
+        catch (\Exception $e)
+        {
+            print_r((array)$post);
+            exit;
+            $display = CHRISTINA_404;
+        }
+        
+        if (Query::isJson())
+        {
+            $response = array(
+                'version' => CHRISTINA_VERSION,
+                'success' => !!$post,
+                'display' => $display,
+                'raw' => $postArray,
+                'tags' => $tags
+            );
+            $response['blacklisted'] = Blacklists::check($tags, $blacklist);
+            $json = json_encode($response, JSON_NUMERIC_CHECK | JSON_PRETTY_PRINT);
+            echo $json;
+        }
+        else
+        {
+            if ($post)
             {
-                Response::showBlacklistedImage();
+                if (Blacklists::check($tags, $blacklist))
+                {
+                    Response::showBlacklistedImage();
+                    exit();
+                }
+                else
+                {
+                    header("Location: $display");
+                }
+
+                Response::someThingsNeverChange();
                 exit();
             }
             else
             {
-                header("Location: $display");
+                header('Location: /404');
+                exit();
             }
-
-            Response::someThingsNeverChange();
-            exit();
-        }
-        else
-        {
-            header('Location: /404');
-            exit();
         }
     }
-}
-else
-{
-    Response::youProbablyDontKnowWhatYouAreDoing();
+    else
+    {
+        Response::youProbablyDontKnowWhatYouAreDoing();
+    }
 }
