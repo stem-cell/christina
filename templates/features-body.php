1b7169eade29a55c7d9dae2228842bcd655db72a
stem-cell <hypnohubstemcell@gmail.com>
2013-10-29 17:19:13 -0700

    Put some placeholders on the features page so we can work on the routes and see where it leads us.

---
+++ templates/features-body.php
@@ -0,0 +1,6 @@
+<?php namespace Christina; ?>
+<h1>Feature Support</h1>
+<ul>
+    <li>Complaining about PHP is... supported!</li>
+    <li>Useless rants are: <i>supported</i>.</li>
+</ul>
--- templates/features.php
+++ templates/features.php
@@ -1,1 +1,7 @@
 <?php namespace Christina;
+
+$vars['title'] = 'Feature Detection Information - Christina';
+
+$vars['contents'] = Template::render('features-body', []);
+
+Template::display('boilerplate', $vars);
