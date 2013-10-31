b61b6b51020da2f3c37b13545e27b09640f4b384
stem-cell <hypnohubstemcell@gmail.com>
2013-10-31 10:56:18 -0700

    Feature support page body markup.

--- templates/features-body.php
+++ templates/features-body.php
@@ -1,6 +1,8 @@
 <?php namespace Christina; ?>
 <h1>Feature Support</h1>
 <ul>
-    <li>Complaining about PHP is... supported!</li>
-    <li>Useless rants are: <i>supported</i>.</li>
+    <li>APC: <b><?php if(!Features::apc()) echo 'not '; ?>supported</b></li>
+    <li>Memcache: <b><?php if(!Features::memcache()) echo 'not '; ?>supported</b></li>
+    <li>Memcached: <b><?php if(!Features::memcached()) echo 'not '; ?>supported</b></li>
+    <li>Ranting about PHP: <b><?php if(!T_PAAMAYIM_NEKUDOTAYIM) echo 'not '; ?>supported</b></li>
 </ul>
