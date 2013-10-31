62591c040048ef802eb33f75cbf5d566ce764dfe
stem-cell <hypnohubstemcell@gmail.com>
2013-10-31 16:10:33 -0700

    Code quality enhancements to templates.

--- templates/boilerplate.php
+++ templates/boilerplate.php
@@ -7,7 +7,7 @@
 </head>
 
 <body>
-<?php echo $contents; ?>
+<?= $contents ?>
 </body>
 
 </html>
--- templates/features-body.php
+++ templates/features-body.php
@@ -1,8 +1,16 @@
-<?php namespace Christina; ?>
+<?php namespace Christina;
+
+$features = [
+    'APC' => Features::apc(),
+    'Memcache' => Features::memcache(),
+    'Memcached' => Features::memcached(),
+    'Ranting about PHP' => defined('T_PAAMAYIM_NEKUDOTAYIM')
+];
+
+?>
 <h1>Feature Support</h1>
 <ul>
-    <li>APC: <b><?php if(!Features::apc()) echo 'not '; ?>supported</b></li>
-    <li>Memcache: <b><?php if(!Features::memcache()) echo 'not '; ?>supported</b></li>
-    <li>Memcached: <b><?php if(!Features::memcached()) echo 'not '; ?>supported</b></li>
-    <li>Ranting about PHP: <b><?php if(!T_PAAMAYIM_NEKUDOTAYIM) echo 'not '; ?>supported</b></li>
+<?php foreach ($features as $name => $support): ?>
+    <li><?= $name ?>: <b><?php if(!$support) echo 'not '; ?>supported</b></li>
+<?php endforeach; ?>
 </ul>
