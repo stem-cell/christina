8eef5648e9626e8039f66a60ec51c6dd42cc6e6d
stem-cell <hypnohubstemcell@gmail.com>
2013-11-17 07:33:11 -0800

    LESS compilation and CSS minification on build.

--- build.sh
+++ build.sh
@@ -1,5 +1,21 @@
 #!/bin/sh
+
+# Compile LESS into CSS and minify.
+echo -n Compiling and minifying CSS
+for less in less/*.less
+do
+    file=${less##*/}
+    name=${file%.*}
+    lessc "$less" "css/$name.css"
+    lessc --clean-css "$less" "css/$name.min.css"
+    echo -n .
+done
+echo " Done."
+
+# Build the Phar archive.
 php build-phar.php
+
+# Copy it to the development server, if any.
 dev="C:/xampp/myimouto/public/christina.phar"
 if [ -f "$dev" ]; then
     cp -f release/christina.phar "$dev"
