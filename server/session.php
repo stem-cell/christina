00d3c0f5ef01b367fb609aa3caabbfd90edf82e7
stem-cell <hypnohubstemcell@gmail.com>
2013-11-19 14:53:17 -0800

    Session helpers.

---
+++ server/session.php
@@ -0,0 +1,33 @@
+<?php namespace Christina;
+
+// Abstracts handling sessions.
+class Session
+{
+    // Indicates whether or not the session has been started.
+    $started = false;
+
+    // Starts the session, if it hasn't been started already.
+    static function init()
+    {
+        if (!Session::$started)
+        {
+            session_name('_moebooru_session_id'); // Yes, we're hijacking it.
+            session_start();
+        }
+    }
+
+    // Gets a value from the session, or null if it's not present.
+    // Also starts the session if it hasn't been started.
+    static function get($name)
+    {
+        Session::init();
+        return $_SESSION[$name];
+    }
+
+    // Same as above, but for setting values.
+    static function set($name, $value)
+    {
+        Session::init();
+        $_SESSION[$name] = $value;
+    }
+}
