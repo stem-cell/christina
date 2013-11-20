76ac5323258ad0389d7a2b8774a87dde766c47fb
stem-cell <hypnohubstemcell@gmail.com>
2013-11-20 08:06:11 -0800

    Remove old code.

--- server/logic.php
+++ server/logic.php
@@ -1,70 +1,20 @@
 <?php namespace Christina;
 
-// Main logic. Let's do our thing.
-if (Routes::isValid())
+// This file used to contain more code. And it was a mess.
+// Now it's more of a wrapper for the entry point, our "main()".
+// Isn't it nice when things are this clean?
+class Logic
 {
-    Routes::call();
-}
-else
-{
-    if (Query::isPost())
+    // Start Christina.
+    static function perform()
     {
-        $id = intval($_GET['post']);
-        $post = Posts::get($id);
-        $blacklist = BlackLists::get(@$_COOKIE['user_id']);
-        $postArray = $post ? (array)$post : array();
-        $tags = Posts::getTags($postArray);
-        
-        try
+        if (Routes::isValid())
         {
-            $display = Posts::imageUrl((array)$post);
-        }
-        catch (\Exception $e)
-        {
-            print_r((array)$post);
-            exit;
-            $display = CHRISTINA_404;
-        }
-        
-        if (Query::isJson())
-        {
-            $response = array(
-                'version' => CHRISTINA_VERSION,
-                'success' => !!$post,
-                'display' => $display,
-                'raw' => $postArray,
-                'tags' => $tags
-            );
-            $response['blacklisted'] = Blacklists::check($tags, $blacklist);
-            $json = json_encode($response, JSON_NUMERIC_CHECK | JSON_PRETTY_PRINT);
-            echo $json;
+            Routes::call();
         }
         else
         {
-            if ($post)
-            {
-                if (Blacklists::check($tags, $blacklist))
-                {
-                    Response::showBlacklistedImage();
-                    exit();
-                }
-                else
-                {
-                    header("Location: $display");
-                }
-
-                Response::someThingsNeverChange();
-                exit();
-            }
-            else
-            {
-                header('Location: /404');
-                exit();
-            }
+            Response::youProbablyDontKnowWhatYouAreDoing();
         }
-    }
-    else
-    {
-        Response::youProbablyDontKnowWhatYouAreDoing();
     }
 }
