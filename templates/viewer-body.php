f2ca1cb8af59ce3719c3f0f801d1c9052c36c0cb
stem-cell <hypnohubstemcell@gmail.com>
2013-11-21 14:08:44 -0800

    Basic navigation of the posts viewer.

--- templates/viewer-body.php
+++ templates/viewer-body.php
@@ -1,4 +1,14 @@
 <?php namespace Christina; ?>
 <h1><b>Christina</b> Post Viewer</h1>
-
+<div class="viewer">
+    <a class="nav-button left" href="<?= Routes::url('viewer/1'); ?>" title="Previous post">
+        <span>◄</span>
+    </a>
+    <div class="iframe-wrap">
+        <iframe src="<?= Routes::url('post/2.html'); ?>" frameborder="0"></iframe>
+    </div>
+    <a class="nav-button right" href="<?= Routes::url('viewer/3'); ?>" title="Next post">
+        <span>►</span>
+    </a>
+</div>
 <footer>This is a footer! Really!</footer>
